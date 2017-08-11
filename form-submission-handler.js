afe1797ac49e0590d2fb99e8c10913633195cbdf
bremiber <bremiber@espol.edu.ec>
2017-08-11 14:12:29 -0500

    envio de correo
    envio de correo para el proyecto de DAW

---
+++ form-submission-handler.js
@@ -0,0 +1,74 @@
+
+function validEmail(email) { // see:
+  var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+  return re.test(email);
+}
+// Obtener los datos y retornarlos en objeto
+function getFormData() {
+  var elements = document.getElementById("gform").elements; // Todos los elementos del formulario
+
+  var fields = Object.keys(elements).map(function(k) {
+    if(elements[k].name !== undefined) {
+      return elements[k].name;
+    // Caso especial html/edge
+    }else if(elements[k].length > 0){
+      return elements[k].item(0).name;
+    }
+  }).filter(function(item, pos, self) {
+    return self.indexOf(item) == pos && item;
+  });
+
+  var data = {};
+  fields.forEach(function(k){
+    data[k] = elements[k].value;
+    var str = ""; 
+    if(elements[k].type === "checkbox"){ // Caso especial html/edge
+      str = str + elements[k].checked + ", "; 
+      data[k] = str.slice(0, -2); 
+    }else if(elements[k].length){
+      for(var i = 0; i < elements[k].length; i++){
+        if(elements[k].item(i).checked){
+          str = str + elements[k].item(i).value + ", "; 
+          data[k] = str.slice(0, -2);
+        }
+      }
+    }
+  });
+  console.log(data);
+  return data;
+}
+
+function handleFormSubmit(event) {  
+  event.preventDefault();           
+  var data = getFormData();         
+  if( !validEmail(data.email) ) {   
+    document.getElementById('email-invalid').style.display = 'block';
+    return false;
+  } else {
+    var url = event.target.action;  //
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', url);
+    // xhr.withCredentials = true;
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onreadystatechange = function() {
+        console.log( xhr.status, xhr.statusText )
+        console.log(xhr.responseText);
+        document.getElementById('gform').style.display = 'none'; // hide form
+        document.getElementById('thankyou_message').style.display = 'block';
+        return;
+    };
+    // url encode form data for sending as post data
+    var encoded = Object.keys(data).map(function(k) {
+        return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    }).join('&')
+    xhr.send(encoded);
+  }
+}
+
+function loaded() {
+  console.log('contact form submission handler loaded successfully');
+  // bind to the submit event of our form
+  var form = document.getElementById('gform');
+  form.addEventListener("submit", handleFormSubmit, false);
+};
+document.addEventListener('DOMContentLoaded', loaded, false);
